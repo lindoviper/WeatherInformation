90a1dacc6d643bf8a0142790d140b98deeff4277
lindoviper <lindoviper@gmail.com>
2018-12-11 20:18:08 +0200

    initial commit

---
+++ WeatherInformationData/WeatherService/WeatherInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+namespace WeatherService
+{
+    [DataContract]
+    public class WeatherInfo
+    {
+        [DataMember]
+        public string City { get; set; }
+
+        [DataMember]
+        public int Temperature { get; set; }
+
+        [DataMember]
+        public string Forecast { get; set; }
+    }
+}
---
+++ WeatherInformationData/WeatherService/WeatherService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WeatherService
+{
+
+    public class WeatherService : IWeatherService
+    {
+        public WeatherInfo GetWeatherInfo(string value)
+        {
+            return new WeatherInfo() { City = value, Temperature = 20, Forecast = "Rainy" };
+        }
+    }
+}
